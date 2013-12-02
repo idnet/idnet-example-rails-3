0202cdb088d596aae12377136ed71a8d80b2fb68
Pavlo Mashchak <pmaschak@heliostech.hk>
2013-12-02 13:49:53 +0200

    HOTFIX for Merchant callback   * added transation_id param

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -6,13 +6,13 @@ class CheckoutController < ApplicationController
              currency: 'EUR',
              usage: Forgery::Name.company_name,
              transaction_id: SecureRandom.hex,
-             timestamp: Time.now.to_s }.with_indifferent_access
+             timestamp: Time.now.to_i.to_s }.with_indifferent_access
     @params = hash.merge hmac: hsign.sign(hash)
   end
 
   def show
     if hsign.verify?(order_params.merge(_hmac: params[:hmac]).with_indifferent_access)
-      render text: "Order was updated Status: #{params[:status]} - Usage: #{params[:usage]} - Amount: #{params[:amount]} - Id: #{params[:transaction_unique_id]}", status: 200
+      render text: "Order was updated Status: #{params[:status]} - Usage: #{params[:usage]} - Amount: #{params[:amount]} - Id: #{params[:transaction_id]}", status: 200
     else
       render text: 'Hmac is not valid', status: 404
     end
@@ -21,7 +21,7 @@ class CheckoutController < ApplicationController
   private
 
   def order_params
-    params.slice(:amount, :currency, :usage, :status, :timestamp, :transaction_unique_id)
+    params.slice(:amount, :currency, :usage, :status, :timestamp, :transaction_unique_id, :transaction_id)
   end
 
   def hsign
