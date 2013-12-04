bf4dc3fb9ae0cd5b1f26a9f696a7fed8c80c81fa
Hery Ramihajamalala <hery@heliostech.hk>
2013-12-04 15:33:01 +0100

    Adding checkout path

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ IdnetRails::Application.routes.draw do
   match "/signout" => "sessions#destroy", :as => :signout
   match '/private', to: "home#private_data", as: :private
   resources :activities, only: [:index, :create]
-  match '/checkout', to: 'checkout#index'
+  match '/checkout', to: 'checkout#index', as: :checkout
   match '/merchant/callback', to: 'checkout#show'
 end
