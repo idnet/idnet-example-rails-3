e3f7730784cdbc0cc7166111236d50f1b9ebd249
Michael Serdjuk <thePooh.12@gmail.com>
2014-03-05 17:42:20 +0200

    Remade application requests logic

--- app/controllers/app_requests_controller.rb
+++ app/controllers/app_requests_controller.rb
@@ -2,10 +2,15 @@ require 'net/http'
 
 class AppRequestsController < ApplicationController
   def index
-    @application_requests = JSON.parse(access_token.get("/api/v1/json/application_requests/#{current_user.uid}").body)
   end
 
   def callback
-    render text: params.inspect, layout: true
+    @request = JSON.parse(access_token.get("/api/v1/json/request-#{params[:request_id]}_#{current_user.uid}").body)
+  end
+
+  def delete
+    access_token.delete("/api/v1/json/request-#{params[:request_id]}_#{current_user.uid}")
+    flash[:notice] = "Request successfully trashed"
+    redirect_to root_path
   end
 end
